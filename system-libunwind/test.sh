ce99de710a76913ce3d2b99b0d351301c6803141
Radka Janek <radka.janek@redhat.com>
2018-05-28 15:51:07 +0200

    +system-libunwind

---
+++ system-libunwind/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ldd $(dirname $(readlink -f $(which dotnet)))/shared/Microsoft.NETCore.App/*/libcoreclr.so | grep 'libunwind.so'
+if [ $? -eq 1 ]; then
+  echo "libunwind not found"
+  exit 1
+fi
+
+ldd $(dirname $(readlink -f $(which dotnet)))/shared/Microsoft.NETCore.App/*/libcoreclr.so | grep 'libunwind-x86_64.so'
+if [ $? -eq 1 ]; then
+  echo "libunwind-x86_64 not found"
+  exit 1
+fi
+
+echo "system-libunwind PASS"
+
