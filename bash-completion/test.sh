2549ca503ad11c45508711408141d2d8a08bae8b
Radka Janek <radka.janek@redhat.com>
2018-05-28 19:09:09 +0200

    +bash-completion

---
+++ bash-completion/test.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+
+../../get-completions.sh dotnet n | grep new
+if [ $? -eq 1 ]; then
+  echo 'Bash completion "dotnet n" FAIL'
+  exit 1
+fi
+
+../../get-completions.sh dotnet pac | grep pack
+if [ $? -eq 1 ]; then
+  echo 'Bash completion "dotnet pac" FAIL'
+  exit 1
+fi
+
+../../get-completions.sh dotnet mig | grep migrate
+if [ $? -eq 1 ]; then
+  echo 'Bash completion "dotnet mig" FAIL'
+  exit 1
+fi
+
+echo "Bash completion PASS"
+
