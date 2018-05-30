b76b8d282eb63f087d7c301fc97e57d224a66407
Radka Janek <radka.janek@redhat.com>
2018-05-30 18:08:19 +0200

    *system-libcurl: more verbosity

--- system-libcurl/test.sh
+++ system-libcurl/test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 
+echo "Looking for libssl..."
 ldd $(dirname $(readlink -f $(which dotnet)))/shared/Microsoft.NETCore.App/*/System.Net.Http.Native.so | grep 'libssl.so'
 if [ $? -eq 1 ]; then
   echo "libssl not found"
   exit 1
 fi
 
+echo "Looking for libcurl..."
 ldd $(dirname $(readlink -f $(which dotnet)))/shared/Microsoft.NETCore.App/*/System.Net.Http.Native.so | grep -E 'libcurl.so|libcurl-libcurl-httpd24.so'
 if [ $? -eq 1 ]; then
   echo "libcurl not found"
