8beba0bbac9125ac908bd736bcc8fd97c51285b5
Omair Majid <omajid@redhat.com>
2018-10-25 17:51:47 -0400

    Test CLI telemetry is disabled by default
    There are 2 tests included here.
    
    test.sh is an automated/simple test. It runs by default. It can not
    distinguish between a connection to a telemetry server and a
    connection to a nuget server.
    
    test-telemetry-tcpdump.sh is not run at all. But it is more
    comprehensive and requires root. So it's disabled. It can distinguish
    between connections to a telemetry server and connections to nuget
    servers. It works for all commands rather than just those that do
    not connect to nuget.org. I found it useful for manual testing.

---
+++ telemetry-is-off-by-default/test-telemetry-tcpdump.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# This test is *NOT* executed by default.
+
+set -euo pipefail
+set -x
+
+# make sure all sudo invocations later on will work
+sudo echo
+
+# found via experimentation
+telemetry_host=dc.services.visualstudio.com
+
+echo "testing telemetry data"
+host ${telemetry_host}
+sudo tcpdump host "${telemetry_host}" -vv >tcp.out 2>tcp.err &
+sleep 2
+rm -rf WebTest
+mkdir -p WebTest
+pushd WebTest
+dotnet new web >/dev/null 2>&1 || true
+dotnet publish
+popd
+sleep 2
+sudo pkill tcpdump
+sleep 2
+
+cat tcp.out
+cat tcp.err
+
+# tcp dump writes out 1-byte files when no data is captured
+if [[ $(wc -c tcp.out | awk '{print $1}') != 1 ]]; then
+    echo "error: expected no captured packets"
+    exit 3
+fi
+
+echo "testing telemetry data is still sent to the same host"
+host ${telemetry_host}
+sudo tcpdump host "${telemetry_host}" -vv >tcp.out 2>tcp.err &
+sleep 2
+export DOTNET_CLI_TELEMETRY_OPTOUT=0
+dotnet new globaljson >/dev/null 2>&1 || true
+sleep 2
+sudo pkill tcpdump
+sleep 2
+
+cat tcp.out
+cat tcp.err
+
+# tcp dump writes out 1-byte files when no data is captured
+if [[ $(wc -c tcp.out | awk '{print $1}') == 1 ]]; then
+    echo "error: expected captured telemetry packets. is the telemetry being sent to a new location?"
+    exit 4
+fi
+
+echo "OK"
