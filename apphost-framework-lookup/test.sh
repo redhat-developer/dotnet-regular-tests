a76fb66324c338bed9b6672d2edb5eab4669076e
Radka Janek <radka.janek@redhat.com>
2019-04-29 13:00:34 +0200

    apphost-framework to include self contained

--- apphost-framework-lookup/test.sh
+++ apphost-framework-lookup/test.sh
@@ -3,9 +3,17 @@
 set -euo pipefail
 set -x
 
-mkdir bin
-cd bin
+test_publish()
+{
+  name=$1
+  mkdir $name && pushd $name
+  dotnet new console
+  dotnet publish -r linux-x64 --self-contained $2
+  ./bin/Debug/netcoreapp*/linux-x64/publish/$name
+  popd
+}
 
-dotnet new console
-dotnet publish -r linux-x64 --self-contained false
-./bin/Debug/netcoreapp*/linux-x64/publish/bin
+test_publish "self-contained" true
+test_publish "framework-dependent" false
+
+echo "PASS"
