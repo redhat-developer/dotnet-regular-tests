372ae4c0ee715ba81d6375bfe253e3a48aecf982
Omair Majid <omajid@redhat.com>
2019-05-18 16:31:39 -0400

    Use a local version of get-completions.sh
    No point forcing the test framework to have this file. There's nothing
    special about it.

--- bash-completion/test.sh
+++ bash-completion/test.sh
@@ -1,20 +1,23 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 set -euo pipefail
 
-../../get-completions.sh dotnet n | grep new
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+
+"$DIR"/get-completions.sh dotnet n | grep new
 if [ $? -eq 1 ]; then
   echo 'Bash completion "dotnet n" FAIL'
   exit 1
 fi
 
-../../get-completions.sh dotnet pac | grep pack
+"$DIR"/get-completions.sh dotnet pac | grep pack
 if [ $? -eq 1 ]; then
   echo 'Bash completion "dotnet pac" FAIL'
   exit 1
 fi
 
-../../get-completions.sh dotnet mig | grep migrate
+"$DIR"/get-completions.sh dotnet mig | grep migrate
 if [ $? -eq 1 ]; then
   echo 'Bash completion "dotnet mig" FAIL'
   exit 1
