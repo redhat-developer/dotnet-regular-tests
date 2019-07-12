f36ef5808d41c0e1de50f795ef3b7c6140b6d0de
Omair Majid <omajid@redhat.com>
2019-07-12 19:10:57 -0400

    Look for check-ngen-dlls script in the test directory

--- dlls-in-release-mode/test.sh
+++ dlls-in-release-mode/test.sh
@@ -8,11 +8,13 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+script_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 dotnet_dir=$(dirname "$(readlink -f "$(which dotnet)")")
 
-find "${dotnet_dir}" -iname '*.dll' -type f -print0 | xargs -0 ./check-ngen-dlls
+find "${dotnet_dir}" -iname '*.dll' -type f -print0 | xargs -0 "${script_directory}/check-ngen-dlls"
 
-if find "${dotnet_dir}" -iname '*.dll' -type f -print0 | xargs -0 ./check-ngen-dlls | grep 'wBuild: 0x0'; then
+if find "${dotnet_dir}" -iname '*.dll' -type f -print0 | xargs -0 "${script_directory}/check-ngen-dlls" | grep 'wBuild: 0x0'; then
     echo "Found some dlls built in debug mode"
     exit 1
 fi
