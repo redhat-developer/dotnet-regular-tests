b8d3e4ef8b1af984c8354333b89df1219eaab9ff
Omair Majid <omajid@redhat.com>
2019-08-28 12:46:05 -0400

    Add a test to `dotnet restore` with specific RIDs
    See https://github.com/dotnet/source-build/issues/1202

---
+++ restore-with-rid/test.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+set -euo pipefail
+set -x
+
+source /etc/os-release
+
+declare -A archmap
+archmap=(
+    ["aarch64"]="arm64"
+    ["amd64"]="x64"
+    ["armv8b"]="x64"
+    ["armv8l"]="x64"
+    ["i686"]="x86"
+    ["i386"]="x86"
+    ["x86_64"]="x64"
+)
+
+arch=${archmap["$(uname -m)"]}
+
+dotnet new console --no-restore
+dotnet restore -r ${ID}.${VERSION_ID}-${arch}
