d33fc2c6c095b473edc4a1c9d89f6ea051991a22
Omair Majid <omajid@redhat.com>
2019-09-30 13:01:52 -0400

    Fix Runtime ID computation on RHEL
    RHEL 7.7, for example has a VERSION_ID of "7.7". Using the computed
    runtime id of "rhel.7.7-x64" doesn't work, and doesn't seem correct
    either.
    
    Centralize the logic used to generate Runtime IDs and handle RHEL
    there, specially.

--- publish-ready-to-run/test.sh
+++ publish-ready-to-run/test.sh
@@ -3,20 +3,5 @@
 set -euo pipefail
 set -x
 
-source /etc/os-release
-
-declare -A archmap
-archmap=(
-    ["aarch64"]="arm64"
-    ["amd64"]="x64"
-    ["armv8b"]="x64"
-    ["armv8l"]="x64"
-    ["i686"]="x86"
-    ["i386"]="x86"
-    ["x86_64"]="x64"
-)
-
-arch=${archmap["$(uname -m)"]}
-
 dotnet new console --no-restore
-dotnet publish -r ${ID}.${VERSION_ID}-${arch} /p:PublishReadyToRun=true
+dotnet publish -r "$(../runtime-id)" /p:PublishReadyToRun=true
