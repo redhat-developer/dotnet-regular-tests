84edb9c4e59aca5ea239ca0743b58969614a6db3
Omair Majid <omajid@redhat.com>
2020-01-14 17:32:19 -0500

    Add tests to make sure binaries are hardened
    As a bonus, this allows us to verify that the binaries were compiled by
    us and are not prebuilts accidentally leaked into our builds.

---
+++ hardened-binaries/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+root=$(dirname "$(readlink -f "$(command -v dotnet)")")
+echo ".NET Core base directory: $root"
+
+# TODO handle more architectures can just x86-64
+
+file_list=$(find "$root/" -type f -exec file {} \; | grep 'ELF 64-bit LSB' | cut -d: -f 1 | sort -u)
+mapfile -t binaries <<< "$file_list"
+for binary in "${binaries[@]}"; do
+    echo "$binary"
+    # Check for full Relocation Read-Only (aka RELRO)
+    readelf -l "$binary" | grep GNU_RELRO
+    readelf -d "$binary" | grep BIND_NOW
+done
