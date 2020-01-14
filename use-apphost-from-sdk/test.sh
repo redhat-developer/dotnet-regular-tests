34599f547f00d20fbf096da86545ad013ea24e69
Omair Majid <omajid@redhat.com>
2020-01-14 18:02:39 -0500

    Verify the host from the SDK is usable
    This is the other side of
    https://github.com/dotnet/source-build/issues/1202. In this case the
    apphost is not known and the SDK downloads the host from nuget.org
    instead of using the apphost that is included with the sdk.

---
+++ use-apphost-from-sdk/test.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+rm -rf console
+mkdir console
+pushd console
+
+# Verify that the apphost from the SDK is usable and nuget is not
+# needed for building a HelloWorld console application.
+
+cat > nuget.config <<EOF
+<?xml version="1.0" encoding="utf-8"?>
+<configuration>
+  <packageSources>
+    <clear />
+  </packageSources>
+</configuration>
+EOF
+
+dotnet new console
+dotnet build --no-restore
+netcoreapp=( bin/Debug/netcoreapp* )
+"${netcoreapp[0]}"/console
+
+popd
