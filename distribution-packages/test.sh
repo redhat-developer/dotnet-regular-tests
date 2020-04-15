bfa7129dcc69b9063309e896829fcb00ce76bd86
Omair Majid <omajid@redhat.com>
2020-04-15 17:28:47 -0400

    Add a new test to check packages meet the package layout docs

---
+++ distribution-packages/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+sdk_version="$(dotnet --version)"
+runtime_version="$(dotnet --list-runtimes | head -1 | awk '{ print $2 }')"
+runtime_id=$(../runtime-id)
+# This might be the final/only netstandard version from now on
+netstandard_version=2.1
+
+./test-standard-packages \
+    "${runtime_id}" \
+    "${runtime_version}" "${runtime_version}" \
+    "${sdk_version}" \
+    "${netstandard_version}"
