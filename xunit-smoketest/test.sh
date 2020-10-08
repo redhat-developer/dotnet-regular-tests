3386efc6ed6cc7050ce114fdeb1e04e24fce625c
Tom Deseyn <tom.deseyn@gmail.com>
2020-10-08 17:43:29 +0200

    Add xunit smoke test

---
+++ xunit-smoketest/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for TEST_RESULT in pass fail ;
+do
+  export TEST_RESULT
+  OUTPUT=$(dotnet test 2>&1)
+  EXIT_CODE=$?
+  if [[ ( "$TEST_RESULT" == "pass" && "$EXIT_CODE" != "0" ) \
+     || ( "$TEST_RESULT" == "fail" && "$EXIT_CODE" == "0" ) ]] ; then
+    echo "$OUTPUT"
+    echo "xunit tests can $TEST_RESULT: FAIL"
+    exit 1
+  fi
+  echo "xunit tests can $TEST_RESULT: PASS"
+done
