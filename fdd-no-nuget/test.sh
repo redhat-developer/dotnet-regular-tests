197adfd4880f0a141bde7fff52268cb3a370a99d
Tom Deseyn <tom.deseyn@gmail.com>
2021-10-28 09:04:08 +0200

    Check offline framework-dependent deployment (#174)

---
+++ fdd-no-nuget/test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+set +x
+
+rm -rf ~/.nuget
+
+  # Create the nuget.config with <clear /> to block NuGet
+cat <<EOF >nuget.config
+<?xml version="1.0" encoding="utf-8"?>
+<configuration>
+  <packageSources>
+    <clear />
+  </packageSources>
+</configuration>
+EOF
+
+dotnet publish
+
+if [ $? -eq 1 ]; then
+  echo "FAIL: unable to publish FFD without nuget.org access"
+  exit 1
+fi
+
+echo "PASS: successfully published FDD without nuget.org access"
