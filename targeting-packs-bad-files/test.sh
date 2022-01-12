3f5eb9dd3216d7db5ba988578798db875a152144
Omair Majid <omajid@redhat.com>
2022-01-12 10:52:29 -0500

    Add a test to make sure there no bad files under packs/
    Fixes: #107

---
+++ targeting-packs-bad-files/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -euo pipefail
+
+set -x
+
+dotnet_dir=$(dirname "$(readlink -f "$(command -v dotnet)")")
+echo "Found .NET at $dotnet_dir"
+
+errors=0
+
+json_lines=$(find "$dotnet_dir/packs" -iname '*.json' | wc -l)
+if [[ $json_lines -gt 0 ]]; then
+    echo "error: found some json files in $dotnet_dir/packs"
+    find "$dotnet_dir/packs" -iname '*.json'
+    errors=1
+fi
+
+debug_lines=$(find "$dotnet_dir/packs" -iname 'Debug' | wc -l)
+if [[ $debug_lines -gt 0 ]]; then
+    echo "error: found some Debug files in $dotnet_dir/packs"
+    find "$dotnet_dir/packs" -iname 'Debug'
+    errors=1
+fi
+
+if [[ $errors -gt 0 ]]; then
+    exit 1
+fi
