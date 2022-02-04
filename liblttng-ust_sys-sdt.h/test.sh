3d9065261aa7a95a4ef006caf6ee03cde0aa0481
Omair Majid <omajid@redhat.com>
2022-02-04 10:19:32 -0500

    Support new ABI version of the lttng-ust library

--- liblttng-ust_sys-sdt.h/test.sh
+++ liblttng-ust_sys-sdt.h/test.sh
@@ -8,14 +8,14 @@ packageName=$(rpm -qa | grep 'dotnet.*lttng-ust')
 set -e
 # If a dotnet-specific lttng package doesn't exist, we must be using
 # the normal system-wide lttng package.
-if [ -z "$packageName" ]; then
+if [[ -z "$packageName" ]]; then
   packageName=$(rpm -qa | grep 'lttng-ust')
 fi
 
-filePath=$(rpm -ql $packageName | grep 'liblttng-ust.so.0$')
-readelf -n $filePath | grep 'NT_STAPSDT (SystemTap probe descriptors)'
+filePath=$(rpm -ql "$packageName" | grep -E 'liblttng-ust.so.[01]$')
+readelf -n "$filePath" | grep -F 'NT_STAPSDT (SystemTap probe descriptors)'
 
-if [ $? -eq 1 ]; then
+if [[ $? -eq 1 ]]; then
   echo "NO NT_STAPSDT were found in lttng-ust: FAL"
   exit 1
 fi
