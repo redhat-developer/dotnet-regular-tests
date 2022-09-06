e8f852489efe3ec30dc92877afaa8cb5a8cb5da0
Omair Majid <omajid@redhat.com>
2022-09-06 14:11:17 -0400

    Stay in current directory if switching user in workload test
    Otherwise we lose access to the NuGet.config files that are placed in
    the current directory (or a parent directory), which, in turn, means
    that we do not use all the configured nuget package sources correctly.

--- workload/test.sh
+++ workload/test.sh
@@ -4,19 +4,21 @@ set -euo pipefail
 
 set -x
 
-# This test *must* be run as non-root for it to have any meaning. So, force it
-# to re-run itself under a non-root user if it's accidentally run as root.
-if [[ $(id -u) == "0" ]]; then
-    id testrunner || useradd testrunner
-    su - testrunner -c "$(readlink -f "$0")" "$@"
-    exit
-fi
-
 # In some environments, such as Github Actions, /tmp/ is strange and
 # basically reserved for root only. So lets use another directory as
 # /tmp/ for this test for the temporary data. Main workload
 # installation is still to the user's home.
 mkdir -p "$(pwd)/workload-temp/"
+
+# This test *must* be run as non-root for it to have any meaning. So, force it
+# to re-run itself under a non-root user if it's accidentally run as root.
+if [[ $(id -u) == "0" ]]; then
+    id testrunner || useradd testrunner
+    chmod ugo+rw "$(pwd)/workload-temp/"
+    su testrunner -c "$(readlink -f "$0")" "$@"
+    exit
+fi
+
 export TMPDIR="$(pwd)/workload-temp/"
 
 dotnet workload search
