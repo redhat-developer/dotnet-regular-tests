86ad77ce56c8dc926559fe09cbfaff9609a1bc80
Omair Majid <omajid@redhat.com>
2023-06-13 16:40:08 -0400

    Add a test for file descriptor exhaustion
    The mono based runtimes (used on ppc64le/s390x) have a bug where the
    process has a low limit for file descriptors and then exhausts them
    quickly. More at https://github.com/dotnet/runtime/issues/82428. This
    was fixed for .NET 8
    
    Add a check for that.

---
+++ file-descriptor-exhaustion/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace FileDescriptorExhaustion;
+
+public class UnitTest1
+{
+    [Fact]
+    public static void True()
+    {
+        Assert.True(true);
+    }
+}
