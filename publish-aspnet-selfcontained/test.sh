778495b7fd41971a516f2c602cf42de7514460ec
Tom Deseyn <tom.deseyn@gmail.com>
2023-06-21 21:52:07 +0200

    Add tests that publish with the .NET 8 bundled runtime packs. (#264)
    * Add tests that publish with the .NET 8 bundled runtime packs.
    
    * Only use bundled runtimes.

---
+++ publish-aspnet-selfcontained/test.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+
+# Clear nuget sources to use the bundled runtime.
+cat > nuget.config <<EOF
+<?xml version="1.0" encoding="utf-8"?>
+<configuration>
+  <packageSources>
+    <clear />
+  </packageSources>
+</configuration>
+EOF
+
+runtime_id="$(../runtime-id)"
+
+url="http://localhost:5000"
+
+dir=web
+output_dir=output
+rm -rf "$dir" "$output_dir"
+dotnet new web -o "$dir"
+dotnet publish --sc -r "$runtime_id" -o "$output_dir" "$dir" 
+
+ASPNETCORE_URLS="$url" "./$output_dir/web" &
+run_pid=$!
+trap "kill $run_pid && wait $run_pid" EXIT
+
+sleep 5
+
+if ! curl "$url"; then
+  echo 'FAIL: ASP.NET app failed to respond'
+  exit 2
+fi
+
+echo "PASS"
---
+++ publish-dotnet-selfcontained/test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+
+# Clear nuget sources to use the bundled runtime.
+cat > nuget.config <<EOF
+<?xml version="1.0" encoding="utf-8"?>
+<configuration>
+  <packageSources>
+    <clear />
+  </packageSources>
+</configuration>
+EOF
+
+runtime_id="$(../runtime-id)"
+
+dir=console
+output_dir=output
+rm -rf "$dir" "$output_dir"
+dotnet new console -o "$dir"
+dotnet publish --sc -r "$runtime_id" -o "$output_dir" "$dir" 
+"./$output_dir/console"
+
+echo "PASS"
