d7687191ca3e652803d027f0be4a558462f376b1
Omair Majid <omajid@redhat.com>
2023-07-07 13:44:53 -0400

    Add a test to make sure file descriptor limits are correct
    Some versions of the mono based runtimes (used on ppc64le/s390x) are
    know to have a bug where the process has a low limit for file
    descriptors and then exhausts them quickly. It's easy to run into this
    if you use many nuget sources, for example. More at
    dotnet/runtime#82428. This was fixed for .NET 8

---
+++ limits/LimitsTest.cs
@@ -0,0 +1,41 @@
+using System.Diagnostics;
+using System.Globalization;
+using Xunit;
+
+namespace Limit;
+
+public class LimitsTest
+{
+    [Fact]
+    public void FileDescriptorLimitIsAtMax()
+    {
+        int softLimit = int.Parse(RunAndGetProcessOutput("ulimit", new List<string> { "-Sn" }), CultureInfo.InvariantCulture);
+        int hardLimit = int.Parse(RunAndGetProcessOutput("ulimit", new List<string> { "-Hn" }), CultureInfo.InvariantCulture);
+
+        Assert.Equal(hardLimit, softLimit, $"File descriptor soft limit ({softLimit}) should be the same as the hard limit ({hardLimit}).");
+    }
+
+    private static string RunAndGetProcessOutput(string name, List<string> args)
+    {
+        ProcessStartInfo psi = new()
+        {
+            FileName = name,
+            RedirectStandardOutput = true,
+        };
+        foreach (string arg in args)
+        {
+            psi.ArgumentList.Add(arg);
+        }
+
+        Process? p = Process.Start(psi);
+        if (p is not null)
+        {
+            p.WaitForExit();
+            return p.StandardOutput.ReadToEnd();
+        }
+        else
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
