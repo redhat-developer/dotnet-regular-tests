1ecd8f55b9c91b956da328ef230d4dfa061058a5
Tom Deseyn <tom.deseyn@gmail.com>
2023-10-12 11:58:05 +0200

    Update nativeaot test for .NET 8.

--- nativeaot/test.sh
+++ nativeaot/test.sh
@@ -13,14 +13,14 @@ if ! dotnet publish --use-current-runtime /p:PublishAot=true; then
 fi
 
 # Verify the published output contains a single file.
-PUBLISHED_FILE_COUNT=$(ls ./bin/Debug/net*/*/publish | wc -l)
+PUBLISHED_FILE_COUNT=$(ls ./bin/*/net*/*/publish | grep -v console.dbg | wc -l)
 if [ "$PUBLISHED_FILE_COUNT" != "1" ]; then
     echo "FAIL: published NativeAot app contains more than 1 file."
     exit 1
 fi
 
 # Verify the application runs.
-APP_OUTPUT="$(./bin/Debug/net*/*/publish/console 2>&1)"
+APP_OUTPUT="$(./bin/*/net*/*/publish/console 2>&1)"
 if [ "$APP_OUTPUT" != "Hello, World!" ]; then
     echo "FAIL: NativeAot console application did not have expected output."
     exit 1
