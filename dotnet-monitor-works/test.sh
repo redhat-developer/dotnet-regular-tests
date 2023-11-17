c3f7cccf4b56274f34e50ef779857bb0df3a5176
Niall Crowe <nicrowe@redhat.com>
2023-11-17 10:21:10 +0000

    test coverage for dotnet-monitor
    Adding test coverage for the diagnostic tool 'dotnet-monitor'.

---
+++ dotnet-monitor-works/test.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+IFS=$'\n\t'
+
+IFS='.-' read -ra VERSION_SPLIT <<< "$1" 
+dotnet tool update -g dotnet-monitor --version "${VERSION_SPLIT[0]}.*-*"
+
+export PATH="$HOME/.dotnet/tools:$PATH"
+
+dotnet-monitor collect --no-auth &
+sleep 5
+indexhttps=$(wget --no-check-certificate -O https.html https://127.0.0.1:52323/info)
+
+https=$(cat https.html)
+
+if [[ $https == *"version"* ]]; then
+   sleep 5
+   echo "collect - OK"
+else
+   sleep 5
+   pkill dotnet-monitor
+   rm "https.html"
+   echo "collect - FAIL"
+   exit 1
+fi
+
+pkill dotnet-monitor
+rm "https.html"
+
+config=$(dotnet-monitor config show)
+if [[ $config == *"Metrics"* ]]; then
+   echo "config show - OK"
+else
+   echo "config show - FAIL"
+   pkill dotnet-monitor
+   exit 1
+fi
+
+authkey=$(dotnet-monitor generatekey)
+if [[ $authkey == *"Authorization: Bearer"* ]]; then
+   echo "generatekey - OK"
+else
+   echo "generatekey - FAIL"
+   pkill dotnet-monitor
+   exit 1
+fi
